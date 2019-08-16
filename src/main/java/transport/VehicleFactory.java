5d8b42db36d45184552a01026a0b2b1d6bde3777
alexeyrakitsky <alexey.rakitsky@gmail.com>
2019-08-16 11:47:00 +0300

    Update VehicleFactory.java

--- src/main/java/transport/VehicleFactory.java
+++ src/main/java/transport/VehicleFactory.java
@@ -9,6 +9,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class VehicleFactory {
+    
+    //todo Сложно объяснить. Но мне нравится. У тебя тут как-то получается, что фабрика инициализирует весь транспорт.
+    //ИМХО, фабрика должна предоставлять экземпляр по запросу (на основании переданных условий)
+    //Т.е. initVehicle (только лучше наверное createVehicleOfClass) еще соответствует патерну фабрики
+    //а вот initVehicles - уже нет
 
     public static <T> T initVehicle(Class<T> clazz) {
         try {
