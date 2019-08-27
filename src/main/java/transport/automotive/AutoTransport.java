9260a556febabcdd1de942a998033d5c0f2011ab
alexeyrakitsky <alexey.rakitsky@gmail.com>
2019-08-27 13:52:40 +0300

    Update AutoTransport.java

--- src/main/java/transport/automotive/AutoTransport.java
+++ src/main/java/transport/automotive/AutoTransport.java
@@ -10,6 +10,9 @@ import java.util.List;
 public class AutoTransport extends Vehicle {
 
     protected static final List<DestinationType> DESTINATIONS = Collections.unmodifiableList(Arrays.asList(DestinationType.RAILWAYSTATION, DestinationType.OTHER));
+    //todo Это лучше не так делать
+    //1. Если у тебя везде Destinations одинаков для автотранспорта, то ты можешь вообще не запрашивать этот параметр
+    //2. Если как правило он одинаков, но ты хочешь оставить возможность его изменить, то просто сделай два конструктора
 
     public AutoTransport(String vehicleName, int maxRidership, int maxTonnage, int maxSpeed,
                          List<DestinationType> destination) {
